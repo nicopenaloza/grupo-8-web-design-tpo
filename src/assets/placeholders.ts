06470d61204e70b39772a29f4d13ceb06ffdc8f5
Nicolas Peñaloza <nicopenaloza@gmail.com>
2025-11-16 22:10:01 -0300

    images path fix

--- src/assets/placeholders.ts
+++ src/assets/placeholders.ts
@@ -1,31 +1,31 @@
 export const heroImage =
-  '/hall_principal.webp';
+  '/grupo-8-web-design-tpo/hall_principal.webp';
 
 export const promotionImages = {
-  'ropa': '/vidriera.webp',
-  'ropa2': '/moda.webp',
-  'hackaton': '/hackaton.webp',
-  'fotografia': '/fotografia.webp',
-  'jazz': '/jazz.webp',
-  'lectura': '/lectura.webp',
-  'firma-libros': '/firma_libros.webp',
-  'cine': '/cine.webp',
-  'gourmet': '/gourmet.webp'
+  'ropa': '/grupo-8-web-design-tpo/vidriera.webp',
+  'ropa2': '/grupo-8-web-design-tpo/moda.webp',
+  'hackaton': '/grupo-8-web-design-tpo/hackaton.webp',
+  'fotografia': '/grupo-8-web-design-tpo/fotografia.webp',
+  'jazz': '/grupo-8-web-design-tpo/jazz.webp',
+  'lectura': '/grupo-8-web-design-tpo/lectura.webp',
+  'firma-libros': '/grupo-8-web-design-tpo/firma_libros.webp',
+  'cine': '/grupo-8-web-design-tpo/cine.webp',
+  'gourmet': '/grupo-8-web-design-tpo/gourmet.webp'
 };
 
 export const eventBanners = {
-  'hackaton': '/hackaton.webp',
-  'fotografia': '/fotografia.webp',
-  'jazz': '/jazz.webp',
-  'lectura': '/lectura.webp',
-  'firma-libros': '/firma_libros.webp',
-  'cine': '/cine.webp'
+  'hackaton': '/grupo-8-web-design-tpo/hackaton.webp',
+  'fotografia': '/grupo-8-web-design-tpo/fotografia.webp',
+  'jazz': '/grupo-8-web-design-tpo/jazz.webp',
+  'lectura': '/grupo-8-web-design-tpo/lectura.webp',
+  'firma-libros': '/grupo-8-web-design-tpo/firma_libros.webp',
+  'cine': '/grupo-8-web-design-tpo/cine.webp'
 }
 
 export const shopLogos: Record<string, string> = {
-  'frezia-amapola': '/frezia_amapola_indumentaria.jpg',
-  'hard-tech': '/hard_tech_logo.jpg',
-  'aura-tech': '/aura_tech_logo.jpg',
-  'greenshot-golf-store': '/greenshot_golf_store.webp',
+  'frezia-amapola': '/grupo-8-web-design-tpo/frezia_amapola_indumentaria.jpg',
+  'hard-tech': '/grupo-8-web-design-tpo/hard_tech_logo.jpg',
+  'aura-tech': '/grupo-8-web-design-tpo/aura_tech_logo.jpg',
+  'greenshot-golf-store': '/grupo-8-web-design-tpo/greenshot_golf_store.webp',
 };
 
